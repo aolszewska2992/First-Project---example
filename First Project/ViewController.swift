6577596b1b0bfad99e11f9f13ec364a8bf93467f
Anna Olszewska <aniaolszewska02@gmail.com>
2020-07-01 18:13:00 +0200

    Initial Commit

---
+++ First Project/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  First Project
+//
+//  Created by Ania Olszewska on 01/07/2020.
+//  Copyright © 2020 Ania Olszewska. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
